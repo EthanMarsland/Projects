4e3da56c4ac98c861ad71a82d5b9b7922b4fe30d
Ethan Marsland <ethanmarsland17@gmail.com>
2018-02-06 17:32:20 +1100

    Moved files and deleted unneeded files

--- TCPConnectionPlugin/Intermediate/Build/Android/UE4/Development/UELinkerFixups/PCH.UELinkerFixups.h
+++
@@ -1,2 +0,0 @@
-// PCH for D:\2017\Assessment\Packaged Plugin\TCPConnectionPlugin\HostProject\Intermediate\Build\Android\UE4\Development\UELinkerFixupsName.h
-#include "D:\2017\Assessment\Packaged Plugin\TCPConnectionPlugin\HostProject\Intermediate\Build\Android\UE4\Development\UELinkerFixupsName.h"
--- TCPConnectionPlugin/Intermediate/Build/Android/UE4/Shipping/UELinkerFixups/PCH.UELinkerFixups.h
+++
@@ -1,2 +0,0 @@
-// PCH for D:\2017\Assessment\Packaged Plugin\TCPConnectionPlugin\HostProject\Intermediate\Build\Android\UE4\Shipping\UELinkerFixupsName.h
-#include "D:\2017\Assessment\Packaged Plugin\TCPConnectionPlugin\HostProject\Intermediate\Build\Android\UE4\Shipping\UELinkerFixupsName.h"
